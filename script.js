7345e85732b209ed1fb7943db60899467341be2f
kirillsvc <kirya.smir17@gmail.com>
2022-07-08 00:35:50 +0700

    generate new number before user inputs an answer

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+let randomNum
+
 function onButtonClicked() {
     const multBy = getById("maxNum").value == "" ? 
         10 : getById("maxNum").value
 
-    const randomNum = Math.floor(Math.random() * multBy + 1)
+    //const randomNum = Math.floor(Math.random() * multBy + 1)
     const out = getById("out")
     const generated = getById("generated")
     const input = getById("inputnum").value
@@ -18,10 +20,20 @@ function onButtonClicked() {
         else out.innerHTML = "Your answer is right"
 
     generated.innerHTML = `The hidden number: ${randomNum}`
+    randomNum = Math.floor(Math.random() * 10 + 1)
 }
 
 const getById = (id) => document.getElementById(id)
 const checkIfEnter = (event) => event.keyCode === 13 
+const onInputFocused = (() => {
+    let exec = false
+    return () => {
+        if(!exec) {
+            exec = true
+            randomNum = Math.floor(Math.random() * 10 + 1)
+        }
+    }
+})()
 
 getById("inputnum").addEventListener("keypress", event => {
     const checkbtn = getById("checkbtn")
